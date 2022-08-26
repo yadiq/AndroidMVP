e09a6146f6f5bd3a22447891b3c4b51fb713f6cf
gyd <guoyadi@live.cn>
2022-08-26 11:34:20 +0800

    修改弹窗布局3

--- app/src/main/java/com/hqumath/androidmvp/utils/DialogUtil.java
+++ app/src/main/java/com/hqumath/androidmvp/utils/DialogUtil.java
@@ -4,12 +4,9 @@ import android.app.Dialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.Button;
-import android.widget.TextView;
 
 import com.hqumath.androidmvp.R;
+import com.hqumath.androidmvp.databinding.DialogCommonBinding;
 
 /**
  * ****************************************************************
@@ -24,15 +21,7 @@ import com.hqumath.androidmvp.R;
 
 public class DialogUtil extends Dialog {
     private Context mContext;
-    private TextView tvTitle;//标题
-    private TextView tvMessage;//内容
-    private Button btnOneYes;//一个按钮时，确定按钮
-    private View llTwo;//两个按钮布局
-    private Button btnYes;//确定按钮
-    private Button btnNo;//取消按钮
-    private View line;//确定取消按钮中间竖线，有些样式没有
-    private View ivRightClose;//右上角叉号
-    private View mView;
+    private DialogCommonBinding binding;
 
     //ex.
         /*DialogUtil alterDialogUtils = new DialogUtil(mContext);
@@ -41,8 +30,6 @@ public class DialogUtil extends Dialog {
         alterDialogUtils.setTwoConfirmBtn("确定", v -> {});
         alterDialogUtils.setTwoCancelBtn("取消", v -> {});
         alterDialogUtils.show();*/
-    //仅显示确定按钮
-    //alterDialogUtils.setOneConfirmBtn("确定", v -> {});
 
     /*
      * 默认主要操作弹窗
@@ -54,14 +41,8 @@ public class DialogUtil extends Dialog {
     public DialogUtil(Context context, int theme, int messageLayout) {
         super(context, theme);
         this.mContext = context;
-        mView = LayoutInflater.from(getContext()).inflate(messageLayout, null);
-        tvTitle = (TextView) mView.findViewById(R.id.title);
-        tvMessage = (TextView) mView.findViewById(R.id.message);
-        btnOneYes = (Button) mView.findViewById(R.id.oneYes);
-        llTwo = mView.findViewById(R.id.llTwo);
-        btnYes = (Button) mView.findViewById(R.id.yes);
-        btnNo = (Button) mView.findViewById(R.id.no);
-        setContentView(mView);
+        binding = DialogCommonBinding.inflate(LayoutInflater.from(mContext));
+        setContentView(binding.getRoot());
         //根布局为自适应宽高，有软键盘时必须全屏，否则mate40等手机软键盘无法上推
         /*Window window = getWindow();
         if (window != null) {
@@ -69,53 +50,61 @@ public class DialogUtil extends Dialog {
         }*/
     }
 
+    public void setTitle(int resId) {
+        binding.tvTitle.setText(resId);
+    }
+
     public void setTitle(String title) {
-        tvTitle.setText(title);
+        binding.tvTitle.setText(title);
     }
 
     public void setMessage(int resId) {
-        tvMessage.setText(resId);
+        binding.tvMessage.setText(resId);
     }
 
     public void setMessage(String message) {
-        tvMessage.setText(message);
+        binding.tvMessage.setText(message);
     }
 
-    public void showRightClose() {
-        if (ivRightClose != null)
-            ivRightClose.setVisibility(View.VISIBLE);
-    }
-
+    /**
+     * 仅显示确定按钮
+     *
+     * @param text
+     * @param listener
+     */
     public void setOneConfirmBtn(String text, View.OnClickListener listener) {
         setOneOrTwoBtn(true);
         if (text != null) {
-            btnOneYes.setText(text);
+            binding.btnOneYes.setText(text);
         }
-        btnOneYes.setOnClickListener(v -> {
+        binding.btnOneYes.setOnClickListener(v -> {
             dismiss();
-            listener.onClick(v);
+            if (listener != null)
+                listener.onClick(v);
         });
     }
 
     public void setTwoConfirmBtn(String text, View.OnClickListener listener) {
         setOneOrTwoBtn(false);
         if (text != null) {
-            btnYes.setText(text);
+            binding.btnYes.setText(text);
         }
-        btnYes.setOnClickListener(v -> {
+        binding.btnYes.setOnClickListener(v -> {
             dismiss();
-            listener.onClick(v);
+            if (listener != null)
+                listener.onClick(v);
         });
     }
 
     public void setTwoCancelBtn(String text, View.OnClickListener listener) {
         setOneOrTwoBtn(false);
         if (text != null) {
-            btnNo.setText(text);
+            binding.btnNo.setText(text);
         }
-        btnNo.setOnClickListener(v -> {
+        binding.btnNo.setOnClickListener(v -> {
             dismiss();
-            listener.onClick(v);
+            if (listener != null)
+                listener.onClick(v);
         });
     }
 
@@ -126,15 +115,11 @@ public class DialogUtil extends Dialog {
      */
     private void setOneOrTwoBtn(boolean one) {
         if (one) {
-            if (btnOneYes != null)
-                btnOneYes.setVisibility(View.VISIBLE);
-            if (llTwo != null)
-                llTwo.setVisibility(View.GONE);
+            binding.btnOneYes.setVisibility(View.VISIBLE);
+            binding.llTwo.setVisibility(View.GONE);
         } else {
-            if (btnOneYes != null)
-                btnOneYes.setVisibility(View.GONE);
-            if (llTwo != null)
-                llTwo.setVisibility(View.VISIBLE);
+            binding.btnOneYes.setVisibility(View.GONE);
+            binding.llTwo.setVisibility(View.VISIBLE);
         }
     }
 }
