69b2245467d754fd297a74664fa3610f0580d8d0
gyd <guoyadi@live.cn>
2022-12-16 14:27:55 +0800

    android10 图片裁剪

--- app/src/main/java/com/hqumath/androidmvp/ui/fileupdown/FileUpDownActivity.java
+++ app/src/main/java/com/hqumath/androidmvp/ui/fileupdown/FileUpDownActivity.java
@@ -1,10 +1,14 @@
 package com.hqumath.androidmvp.ui.fileupdown;
 
+import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -23,12 +27,14 @@ import java.io.InputStream;
 
 public class FileUpDownActivity extends BaseActivity implements FileUpDownPresenter.Contract {
     private final int REQUEST_PIC = 0x01;//相册选图
+    private final int REQUEST_CROP = 0x02;//图片裁剪
+    public final static String url = "http://cps.yingyonghui.com/cps/yyh/channel/ac.union.m2/com.yingyonghui.market_1_30063293.apk";
 
     private ActivityFileupdownBinding binding;
     private FileUpDownPresenter mPresenter;
     private DownloadingDialog mDownloadingDialog;
 
-    public final static String url = "http://cps.yingyonghui.com/cps/yyh/channel/ac.union.m2/com.yingyonghui.market_1_30063293.apk";
+    private Uri cropUri;//剪裁时临时文件
 
     @Override
     public View initContentView(Bundle savedInstanceState) {
@@ -87,22 +93,42 @@ public class FileUpDownActivity extends BaseActivity implements FileUpDownPresen
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
-            if (requestCode == REQUEST_PIC) {//相册
-                if (data != null) {
-                    Uri uri = data.getData();
-                    if (uri != null) {
-                        InputStream inputStream = null;
+            switch (requestCode) {
+                case REQUEST_PIC://相册选图
+                    if (data != null && data.getData() != null) {
+                        //裁剪图片
+                        cropPhoto(data.getData(), false);
+                        //显示图片
+                        /*InputStream inputStream = null;
                         try {
-                            inputStream = getContentResolver().openInputStream(uri);
+                            inputStream = getContentResolver().openInputStream(data.getData());
                             Bitmap mBitmap = BitmapFactory.decodeStream(inputStream);
                             binding.ivPic.setImageBitmap(mBitmap);
                         } catch (Exception e) {
                             e.printStackTrace();
                         } finally {
                             FileUtil.closeStream(inputStream);
-                        }
+                        }*/
                     }
-                }
+                    break;
+                case REQUEST_CROP://图片裁剪
+                    //String picturePath = FileUtil.getRealFilePath(mContext, cropUri);
+                    //显示图片
+                    InputStream inputStream = null;
+                    try {
+                        inputStream = getContentResolver().openInputStream(cropUri);
+                        Bitmap mBitmap = BitmapFactory.decodeStream(inputStream);
+                        //File file = StringUtils.compressImage(mBitmap, 50);//图片压缩
+                        binding.ivPic.setImageBitmap(mBitmap);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    } finally {
+                        FileUtil.closeStream(inputStream);
+                    }
+                    break;
+            }
+            if (requestCode == REQUEST_PIC) {//相册
+
             }
         }
     }
@@ -165,4 +191,47 @@ public class FileUpDownActivity extends BaseActivity implements FileUpDownPresen
         });
     }
 
+    private void cropPhoto(Uri srcUri, boolean fromCamera) {
+        //剪裁时临时文件
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {//android10分区存储
+            ContentValues values = new ContentValues(2);
+            values.put(MediaStore.Images.Media.DISPLAY_NAME, "photo_" + System.currentTimeMillis());
+            values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+            if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {//SD卡是否可用
+                cropUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            } else {
+                cropUri = getContentResolver().insert(MediaStore.Images.Media.INTERNAL_CONTENT_URI, values);
+            }
+        } else {
+            File cropFile;
+            if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {//SD卡是否可用
+                cropFile = FileUtil.getExternalCacheFile("crop.jpg");
+            } else {
+                cropFile = FileUtil.getCacheFile("crop.jpg");
+            }
+            if (cropFile.exists()) {
+                cropFile.delete();
+            }
+            cropUri = Uri.fromFile(cropFile);
+        }
+
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        if (fromCamera) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);//授予临时读写权限
+                intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            }
+        }
+        intent.setDataAndType(srcUri, "image/*");
+        intent.putExtra("corp", "true");//是否可裁剪
+        intent.putExtra("aspectX", 3);//X方向比例
+        intent.putExtra("aspectY", 2);//Y方向比例
+        intent.putExtra("outputX", 750);//裁剪区的宽
+        intent.putExtra("outputY", 500);//裁剪区的高
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, cropUri);//表明这是一个存储动作
+        intent.putExtra("return-data", false);//是否将数据保留在Bitmap中返回
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());//输出格式，一般设为Bitmap格式及图片类型
+        intent.putExtra("noFaceDetection", true);//是否去除面部检测
+        startActivityForResult(intent, REQUEST_CROP);
+    }
 }
--- app/src/main/java/com/hqumath/androidmvp/utils/FileUtil.java
+++ app/src/main/java/com/hqumath/androidmvp/utils/FileUtil.java
@@ -1,6 +1,11 @@
 package com.hqumath.androidmvp.utils;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Environment;
+import android.provider.MediaStore;
 
 import com.hqumath.androidmvp.net.HandlerException;
 
@@ -164,6 +169,34 @@ public class FileUtil {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    /**
+     * 得到图片uri的实际地址
+     */
+    public static String getRealFilePath(final Context context, final Uri uri) {
+        if (null == uri) {
+            return null;
+        }
+        final String scheme = uri.getScheme();
+        String data = null;
+        if (scheme == null) {
+            data = uri.getPath();
+        } else if (ContentResolver.SCHEME_FILE.equals(scheme)) {
+            data = uri.getPath();
+        } else if (ContentResolver.SCHEME_CONTENT.equals(scheme)) {
+            Cursor cursor = context.getContentResolver().query(uri, new String[]{MediaStore
+                    .Images.ImageColumns.DATA}, null, null, null);
+            if (null != cursor) {
+                if (cursor.moveToFirst()) {
+                    int index = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+                    if (index > -1) {
+                        data = cursor.getString(index);
+                    }
+                }
+                cursor.close();
+            }
+        }
+        return data;
     }
 }
